015847e655ae8cedae43597173900a01a76741a1
MounikaKolisetty <mouni.kolisetty@gmail.com>
2024-10-27 07:19:37 -0400

    added footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, MatIconModule],
+  imports: [NavbarComponent, 
+            MatIconModule,
+            FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
