beada2513d2abd920a062cb6ccf36ea83b141312
MounikaKolisetty <mouni.kolisetty@gmail.com>
2024-11-01 14:27:28 -0400

    added footer, nav and content page

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-training',
   standalone: true,
   imports: [NavbarComponent,
-            FooterComponent
+            FooterComponent,
+            RouterOutlet, RouterLink, RouterLinkActive
   ],
   templateUrl: './training.component.html',
   styleUrl: './training.component.css'
