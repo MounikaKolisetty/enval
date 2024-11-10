c107038c5aa04405dd4cdbeaa316e05c9c92be28
MounikaKolisetty <mouni.kolisetty@gmail.com>
2024-11-10 18:05:58 -0500

    home page know page form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { FooterComponent } from '../footer/footer.component';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { RecaptchaModule } from 'ng-recaptcha';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +13,18 @@ import { RecaptchaModule } from 'ng-recaptcha';
             MatIconModule,
             FooterComponent,
             RouterOutlet, RouterLink, RouterLinkActive,
-            RecaptchaModule
+            RecaptchaModule,
+            CommonModule
             ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  isContactVisible: boolean = false; 
+  toggleContact() {
+    this.isContactVisible = !this.isContactVisible; 
+  } 
+  closeContact() {
+    this.isContactVisible = false; 
+  }
 }
